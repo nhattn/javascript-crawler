72d69932ddbf9dd736e48a39b9224c4a46850605
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2009-10-11 05:25:22 +0000

    Save point.

--- trunk/src/main/java/com/zyd/web/core/LinkManager.java
+++ trunk/src/main/java/com/zyd/web/core/LinkManager.java
@@ -1,14 +1,18 @@
 package com.zyd.web.core;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
+import com.zyd.web.dom.Book;
+
 public class LinkManager {
     private static LinkManager instance = new LinkManager();
     private static HashSet<String> links = new HashSet<String>();
+    private static HashMap<String, Book> linkWithBook = new HashMap<String, Book>();
 
     private LinkManager() {
     }
@@ -24,6 +28,33 @@ public class LinkManager {
      */
     public synchronized boolean addLink(String link) {
         return links.add(link);
+    }
+
+    public synchronized boolean addLink(String link, Book book) {
+        if (links.add(link)) {
+            linkWithBook.put(link, book);
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized Book getBookForLink(String link) {
+        return linkWithBook.remove(link);
+    }
+
+    public synchronized boolean addLinks(Collection<String> nlinks) {
+        return LinkManager.links.addAll(nlinks);
+    }
+
+    public synchronized boolean addLinks(Collection<String> nlinks, Book book) {
+        boolean ret = false;
+        for (String s : nlinks) {
+            if (links.add(s)) {
+                linkWithBook.put(s, book);
+                ret = true;
+            }
+        }
+        return ret;
     }
 
     public synchronized String nextLink() {

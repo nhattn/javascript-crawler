4eb346c2497923e383dcb138b5b6c3baa8800176
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-05-14 23:11:20 +0000

    Adding linux ocr with tesseract and imagemagic

--- trunk/thirdparty/tesseract/temp/ocr.sh
+++
@@ -1,5 +0,0 @@
-## convert an image to acceptable format for tesseract
-convert -monochrome /home/yang/workspace/webcrawl/thirdparty/tesseract/temp/num.png /home/yang/workspace/webcrawl/thirdparty/tesseract/temp/num.tif
-
-## run ocr on the new tif file
-tesseract /home/yang/workspace/webcrawl/thirdparty/tesseract/temp/num.tif num

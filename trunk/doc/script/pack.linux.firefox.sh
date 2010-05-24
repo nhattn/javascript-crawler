e457d8fff70a65723e24b45a0fef378a3c891833
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-05-24 07:23:12 +0000

    folder change.

--- trunk/doc/script/pack.linux.firefox.sh
+++
@@ -1,4 +0,0 @@
-rm -f /y/workspace/webcrawl/thirdparty/firefox/crawler-linux-profile.gz
-tar -cf /y/workspace/webcrawl/thirdparty/firefox/crawler-linux-profile.tar -C /y/work/firefox  zyd start.sh
-gzip -c /y/workspace/webcrawl/thirdparty/firefox/crawler-linux-profile.tar > /y/workspace/webcrawl/thirdparty/firefox/crawler-linux-profile.gz
-rm -f /y/workspace/webcrawl/thirdparty/firefox/crawler-linux-profile.tar

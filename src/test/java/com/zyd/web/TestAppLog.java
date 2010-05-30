5cc382d38f5934ca3cd7286ad893ea76f3f7d88e
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-05-30 10:38:06 +0000

    save code

--- src/test/java/com/zyd/web/TestAppLog.java
+++ src/test/java/com/zyd/web/TestAppLog.java
@@ -1,6 +1,8 @@
 package com.zyd.web;
 
 import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,11 +10,12 @@ import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
-
-import junit.framework.TestCase;
 
 import com.tj.common.util.test.CommonTestUtil;
 import com.tj.common.util.test.HttpTestUtil;
@@ -27,9 +30,26 @@ public class TestAppLog extends TestCase {
         assertTrue(ATestUtil.clearServerData());
     }
 
+    public static String httpPut(String urls, Map params, String referer) throws Exception {
+        StringBuffer buf = new StringBuffer(urls);
+        buf.append('?');
+        for (Object o : params.keySet()) {
+            buf.append(o.toString());
+            buf.append('=');
+            buf.append(params.get(o));
+            buf.append('&');
+        }
+        buf.deleteCharAt(buf.length() - 1);
+        URL url = new URL(buf.toString());
+        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+        httpCon.setRequestMethod("PUT");
+        String s = IOUtils.toString(httpCon.getInputStream());
+        return s;
+    }
+
     public void testLog() throws Exception {
         Map values = CommonTestUtil.loadValueMapFromClassPathFile(TestAppLog.class, "applog.prop", Charset.defaultCharset().toString());
-        String s = HttpTestUtil.httpPutForString(ATestConstants.APPLOG_URL, values, null);
+        String s = httpPut(ATestConstants.APPLOG_URL, values, null);//HttpTestUtil.httpPutForString(ATestConstants.APPLOG_URL, values, null);
         assertEquals("ok", s.trim());
         values = new HashMap();
         values.put(Handler.Parameter.PARAMETER_OBJECT_ID, AppLog.name);

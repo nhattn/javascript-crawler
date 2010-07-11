f1fefbaf8b9805b038fa67061f4a4e192d586d47
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-07-11 12:06:12 +0000

    Add for zuiyidong.

---
+++ src/main/java/com/zyd/web/service/poi.java
@@ -0,0 +1,19 @@
+package com.zyd.web.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class poi extends object {
+    @Override
+    public void post(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.get(req, resp);
+    }
+}

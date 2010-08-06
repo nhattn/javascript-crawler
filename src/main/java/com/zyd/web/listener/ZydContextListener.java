40d66c3f272b5bedb77035fb2a3dfef31cd430ba
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-08-06 04:35:23 +0000

    Disable blocking for now.

--- src/main/java/com/zyd/web/listener/ZydContextListener.java
+++ src/main/java/com/zyd/web/listener/ZydContextListener.java
@@ -15,8 +15,8 @@ public class ZydContextListener implements ServletContextListener {
 
     public void contextDestroyed(ServletContextEvent arg0) {
         ((LinkManager) SpringContext.getContext().getBean("linkManager")).stopMonitor();
-        ((IpCounter) SpringContext.getContext().getBean("ipCounter")).stop();
-        ((AccessController) SpringContext.getContext().getBean("accessController")).stopAccessBlocker();
+        //        ((IpCounter) SpringContext.getContext().getBean("ipCounter")).stop();
+        //        ((AccessController) SpringContext.getContext().getBean("accessController")).stopAccessBlocker();
     }
 
     public void contextInitialized(ServletContextEvent arg0) {
@@ -25,8 +25,8 @@ public class ZydContextListener implements ServletContextListener {
         }
         ((LinkManager) SpringContext.getContext().getBean("linkManager")).loadFromDb();
         ((LinkManager) SpringContext.getContext().getBean("linkManager")).startMonitor();
-        ((IpCounter) SpringContext.getContext().getBean("ipCounter")).start();
-        ((AccessController) SpringContext.getContext().getBean("accessController")).startAccessBlocker();
+        //        ((IpCounter) SpringContext.getContext().getBean("ipCounter")).start();
+        //        ((AccessController) SpringContext.getContext().getBean("accessController")).startAccessBlocker();
     }
 
 }

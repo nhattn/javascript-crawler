3bdac414877acd70ed73792c6a02269c0cd90432
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-08-18 04:35:18 +0000

    Bug fix.

--- src/main/webapp/js/handler/house/ganji/list.js
+++ src/main/webapp/js/handler/house/ganji/list.js
@@ -21,24 +21,24 @@ function handlerProcess() {
     });
 }
 
-function isTimeExpired() {  
-    var arr = XPath.array(document, "//span[contains(@class,'time')]"), now = new Date();    
+function isTimeExpired() {
+    var arr = XPath.array(document, "//span[contains(@class,'time')]"), now = new Date();
     var maxDifference = CrGlobal.HouseListMaxDifference[CrUtil.getShortestDomain(window.location.host)];
     var errorCount = 0;
     for ( var i = 0; i < arr.length; i++) {
         var s = arr[i].textContent;
         if (!s || s.trim().length == 0) {
             continue;
-        }        
+        }
         var oDate = CrUtil.guessTime(s.trim());
         if (!oDate) {
+            CrUtil.log('Can not parse date string : ' + s);
             errorCount++;
             if (errorCount > 10) {
                 Crawler.attention('Too many error times in list, the last one is: ' + s);
                 return true;
             }
-        }
-        if ((now.getTime() - oDate.getTime()) > maxDifference) {
+        } else if ((now.getTime() - oDate.getTime()) > maxDifference) {
             return true;
         }
     }
--- src/main/webapp/js/handler/house/koubei/list.js
+++ src/main/webapp/js/handler/house/koubei/list.js
@@ -1,50 +1,47 @@
 function handlerProcess() {
-	if(isTimeExpired()==true){		
-		Crawler.log('House list date expried, will stop process');
-		Crawler.action({
-			action:'Goto.Next.Link'
-		});		
-		return;
-	}	
-	
+    if (isTimeExpired() == true) {
+        Crawler.log('House list date expried, will stop process');
+        Crawler.action( {
+            action : 'Goto.Next.Link'
+        });
+        return;
+    }
+
     var info = {
         dataUrl : Crawler.serverUrl + '/service/link',
         path : "//div[@id='bd']//a[@class='yk-av']",
-        nextPagePath: "//div[@id='page-num']//strong/following-sibling::a[1]",
+        nextPagePath : "//div[@id='page-num']//strong/following-sibling::a[1]",
         regex : new RegExp('fang/detail', 'i')
-    };    
-    
+    };
+
     var links = HandlerHelper.getMatchLinks(info.path, info.regex);
-//    console.log(links);
+    //    console.log(links);
     HandlerHelper.storeLinks(links, {
         action : 'Goto.XPath.Link.Href',
         param1 : info.nextPagePath
     });
 }
 
-
-
-
-function isTimeExpired() {  
-    var arr = XPath.array(document, "//p[@class='subInfo']//span"), now = new Date();    
+function isTimeExpired() {
+    var arr = XPath.array(document, "//p[@class='subInfo']//span"), now = new Date();
     var maxDifference = CrGlobal.HouseListMaxDifference[CrUtil.getShortestDomain(window.location.host)];
     var errorCount = 0;
     for ( var i = 0; i < arr.length; i++) {
         var s = arr[i].textContent;
         if (!s || s.trim().length == 0) {
             continue;
-        }        
+        }
         var oDate = CrUtil.guessTime(s.trim());
         if (!oDate) {
+            CrUtil.log('Can not parse date string : ' + s);
             errorCount++;
             if (errorCount > 10) {
                 Crawler.attention('Too many error times in list, the last one is: ' + s);
                 return true;
             }
-        }
-        if ((now.getTime() - oDate.getTime()) > maxDifference) {
+        } else if ((now.getTime() - oDate.getTime()) > maxDifference) {
             return true;
-        }        
+        }
     }
     return false;
 }

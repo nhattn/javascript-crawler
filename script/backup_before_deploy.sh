c645ade277a912afd4d581b6b1b1caebd78afdd9
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-08-23 02:50:03 +0000

    Adding backup script.

--- script/backup_before_deploy.sh
+++ script/backup_before_deploy.sh
@@ -1,12 +1,14 @@
 DATE=`date +%Y-%m-%d_%H-%M-%S`
 BACKUPDIR='/tomcat/backup/'
+
+echo backing up war
 cp /tomcat/zuiyidong/webapps/ROOT.war  ${BACKUPDIR}${DATE}_ROOT.war
 
 echo Input mysql password for root
 mysqldump -uroot -p crawler > ${BACKUPDIR}${DATE}_data.sql
 
 echo Packing data
-echo tar -czvf  ${BACKUPDIR}${DATE}_data.tar  -C ${BACKUPDIR} ${DATE}_data.sql
+tar -czvf  ${BACKUPDIR}${DATE}_data.tar  -C ${BACKUPDIR} ${DATE}_data.sql
 
 echo removing old file
-echo rm  ${BACKUPDIR}${DATE}_data.sql
+rm  ${BACKUPDIR}${DATE}_data.sql

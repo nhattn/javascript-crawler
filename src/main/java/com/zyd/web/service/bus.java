31d72c25af9b6b307a34731625d69286e1f2f415
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-08-31 08:14:18 +0000

    Adding bus

---
+++ src/main/java/com/zyd/web/service/bus.java
@@ -0,0 +1,16 @@
+package com.zyd.web.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class bus extends api {
+
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.get(req, resp);
+    }
+
+}

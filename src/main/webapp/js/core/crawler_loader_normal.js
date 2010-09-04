1f2f02ee5f9c80339ad2e1f2625b06c492f23103
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-09-04 01:26:29 +0000

    Adding javascript debug/normal switch.

---
+++ src/main/webapp/js/core/crawler_loader_normal.js
@@ -0,0 +1,65 @@
+/**
+CrConfig.RemoteLogging = false;
+CrConfig.doAction = false;
+CrConfig.NextLinkWaitTime = 5000000;
+ **/
+CrGlobal = {
+    handlerPath : '/js/handler',
+    restartInterval : 50,
+    setup : function() {
+        var hostDiv = document.getElementById('crawler_set_url');
+        if (!hostDiv) {
+            throw 'no host div, crawler not set up propertly';
+        }
+        for ( var c in CrConfig) {
+            CrGlobal[c] = CrConfig[c];
+        }
+        delete CrConfig;
+        CrGlobal.serverUrl = 'http://' + hostDiv.value;
+        CrGlobal.StoreLinkUrl = CrGlobal.serverUrl + '/service/link';
+        CrGlobal.ObjectCreationUrl = CrGlobal.serverUrl + '/service/object';
+        CrGlobal.RemoteLoggingUrl = CrGlobal.serverUrl + '/service/log';
+        CrGlobal.initJsUrls();
+        CrGlobal.chainLoad(CrGlobal.jsToLoad);
+    },
+    loadJSFile : function(fileurl, callback) {
+        var sf = document.createElement('script');
+        sf.setAttribute("type", "text/javascript");
+        sf.setAttribute("src", fileurl);
+        if (callback)
+            sf.onload = callback;
+        document.getElementsByTagName("head")[0].appendChild(sf);
+    },
+    chainLoad : function(files) {
+        CrGlobal._chainFiles = files;
+        CrGlobal._chainCurrent = 0;
+        CrGlobal._chainLoadFile();
+    },
+    _chainLoadFile : function() {
+        if (!CrGlobal || !CrGlobal._chainFiles || !CrGlobal._chainFiles.length || CrGlobal._chainCurrent >= CrGlobal._chainFiles.length) {
+            delete CrGlobal._chainFiles;
+            delete CrGlobal._chainCurrent;
+            return;
+        }
+        CrGlobal.loadJSFile(CrGlobal._chainFiles[CrGlobal._chainCurrent++], function() {
+            CrGlobal._chainLoadFile();
+        });
+    },
+    initJsUrls : function() {
+        var verstring = '?v=' + CrGlobal.Version;
+        var jsToLoad = [];
+        if (CrGlobal.jsToLoad) {
+            for ( var i = 0; i < CrGlobal.jsToLoad.length; i++) {
+                var js = CrGlobal.jsToLoad[i];
+                if (js.indexOf(':/') > 0) {
+                    jsToLoad.push(js);
+                } else {
+                    jsToLoad.push(CrGlobal.serverUrl + js + verstring);
+                }
+            }
+            delete CrGlobal.jsToLoad;
+        }
+        CrGlobal.jsToLoad = jsToLoad;
+    }
+}
+CrGlobal.setup();

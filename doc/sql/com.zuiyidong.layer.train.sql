2c4dbbf815b88f5c70dfbc63801bf32c33d7da21
yang.jiang.z@gmail.com <yang.jiang.z@gmail.com@362d651a-ab82-11de-ac05-951101a8bf3e>
2010-09-14 06:11:43 +0000

    Adding train module

--- doc/sql/com.zuiyidong.layer.train.sql
+++ doc/sql/com.zuiyidong.layer.train.sql
@@ -63,4 +63,13 @@ create view layer_com_zuiyidong_layer_trainstation_v as select
         ts.* 
         from layer_com_zuiyidong_layer_trainstation ts, layer_com_zuiyidong_layer_train t 
         where ts.trainId = t.id;
-        
+
+
+/**
+these should be run after gps data is crawled from google
+
+update layer_com_zuiyidong_layer_trainstation set lng=110.206263, lat=31.64636, province='湖北', city='白河' where name ='白河东';
+update layer_com_zuiyidong_layer_trainstation set lng=115.021727, lat=27.105207, province='江西省', city='吉安市', district='青原区' where name ='吉安';
+update layer_com_zuiyidong_layer_trainstation set lng=99.065609, lat=39.350361, province='甘肃省', city=null, district=null where name ='清水';
+
+*/        
